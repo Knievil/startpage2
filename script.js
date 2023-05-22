5d0af14a5e453d41b7589e4fd56b18ee0b827556
Knievilkangaroo <Knievilkangaroo@gmail.com>
2023-05-22 08:09:00 -0500

    tryed to do stack shit but failed

---
+++ script.js
@@ -0,0 +1,20 @@
+function fetchImage() {
+        fetch("https://cataas.com/cat", {
+          method: "GET",
+          headers: {
+            "x-rapidapi-key": apiKey,
+            "x-rapidapi-host": "https://cataas.com/cat",
+          },
+        })
+          .then((response) => response.blob())
+          .then((blob) => {
+            const imageUrl = URL.createObjectURL(blob);
+            const imageElement = document.createElement("img");
+            imageElement.src = imageUrl;
+            const container = document.getElementById("image-container");
+            container.appendChild(imageElement);
+          })
+          .catch((error) => console.error(error));
+      }
+      button.addEventListener("click", fetchImage);
+            const button = document.getElementById('fetch-image-button');
