308e553636f42fc816e5d0e8a88b0da60a1049a7
Knievil <105438364+Knievil@users.noreply.github.com>
2023-05-23 14:53:22 -0500

    2 files modified

--- script.js
+++ script.js
@@ -1,14 +1,56 @@
-const api_url = 'https://wttr.in/?format=j1';
+const url = `https://api.thecatapi.com/v1/breeds`;
 
-async function getWeather() {
-	const response = await fetch(api_url);
+const api_key = "live_lug2kdmJewYN12tTkKjOoqTD8kRnFHw4scGqYYYPTXmfIYOBgNCatnJD4nVjgjtk";
 
-	const data = await response.json();
+// a variable to store the information about the breeds
 
-	const weather = data.current_condition[0];
+let storedBreeds = [];
 
-	let temp = weather.temp_C;
+// a function to select a random breed
 
-	document.getElementById('temp').innerHTML = 'temp:' + temp;
+function getRandomInt(min, max) {
+return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-getWeather();
+
+
+// a function to show images and information of the breeds
+function showCatImageAndInformation(index) {
+
+// This will display the image of the cat
+  document.getElementById("cat_image").src = storedBreeds[index].image.url;
+
+// This will get the breed name
+  document.getElementById("breed_name").innerHTML = storedBreeds[index].name;
+
+// This will get the wiki link
+  document.getElementById("wiki_link").href = storedBreeds[index].wikipedia_url;
+
+  document.getElementById("wiki_link").innerHTML =
+    storedBreeds[index].wikipedia_url;  
+
+// This will get the characteristics of the cat
+  document.getElementById("breed_json").textContent =
+    storedBreeds[index].temperament;
+}
+
+
+// a function to retrieve data from the API
+fetch(url, {
+  headers: {
+    "x-api-key": api_key,
+  },
+})
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    // Storing the retrieved data from the API in our variable
+    storedBreeds = data;
+
+
+    // Using the random function to select a specific breed. Then extracting information from that breed
+    showCatImageAndInformation(getRandomInt(0, storedBreeds.length - 1));
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
